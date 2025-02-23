a90b2fc622a96f307a58fbdc22930693845e3940
meyra <meyramaden@gmail.com>
2025-02-23 12:31:28 +0300

    main eklendi

---
+++ 1_S/csharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Eticarett1_
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Kullanici kullanici = new Kullanici();
+            kullanici.SetIsim("Sefa Aras");
+            kullanici.SetMail("sefaaras@ktu.edu.tr");
+
+            Adres adres = new Adres();
+            adres.Ulke = "Turkiye";
+            adres.Il = "Trabzon";
+            adres.Ilce = "Of";
+            kullanici.AdresGuncelle(adres);
+
+            Console.WriteLine(kullanici.ToString());
+
+            adres.PostKodu = 61000;
+            kullanici.AdresGuncelle(adres);
+
+            Console.WriteLine(kullanici.ToString());
+        }
+    }
+
+}
