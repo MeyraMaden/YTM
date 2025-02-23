a2fcc54755040e1f8c05bdc1911e9c04db58c0f2
meyra <meyramaden@gmail.com>
2025-02-23 12:30:48 +0300

    adres.cs eklendi

---
+++ 1_S/csharp/Adres.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eticarett1_
+{
+    public class Adres
+    {
+        private string ulke;
+        private string il;
+        private string ilce;
+        private int postKodu;
+
+        public string Ulke
+        {
+            get { return ulke; }
+            set { ulke = value; }
+        }
+
+        public string Il
+        {
+            get { return il; }
+            set { il = value; }
+        }
+
+        public string Ilce
+        {
+            get { return ilce; }
+            set { ilce = value; }
+        }
+
+        public int PostKodu
+        {
+            get { return postKodu; }
+            set { postKodu = value; }
+        }
+
+        public override string ToString()
+        {
+            return Ulke + " " + Il + " " + Ilce + " " + PostKodu;
+        }
+    }
+
+}
