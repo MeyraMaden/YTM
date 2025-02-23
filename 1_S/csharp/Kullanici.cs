f51a7fbb60fabe56d60b3ec120e797543282be4c
meyra <meyramaden@gmail.com>
2025-02-23 12:29:50 +0300

    kullanici.cs eklendi

---
+++ 1_S/csharp/Kullanici.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eticarett1_
+{
+    public class Kullanici
+    {
+        private string isim;
+        private string mail;
+        private Adres adres;
+
+        public void SetIsim(string isim)
+        {
+            this.isim = isim;
+        }
+
+        public string GetIsim()
+        {
+            return isim;
+        }
+
+        public string GetMail()
+        {
+            return mail;
+        }
+
+        public void SetMail(string mail)
+        {
+            this.mail = mail;
+        }
+
+        public void AdresGuncelle(Adres adres)
+        {
+            this.adres = adres;
+        }
+
+        public string GetAdres()
+        {
+            return adres.ToString();
+        }
+
+        public override string ToString()
+        {
+            return GetIsim() + " " + GetMail() + " " + GetAdres();
+        }
+
+        public void GirisYap(string mail, string sifre)
+        {
+            System.Console.WriteLine("Giriş yapıldı.");
+        }
+    }
+
+}
