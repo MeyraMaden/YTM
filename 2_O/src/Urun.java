9ae49f3976ade46c1563540670ddf45205a39349
meyra <meyramaden@gmail.com>
2025-02-26 17:48:29 +0300

    urun  classi eklendi

---
+++ 2_O/src/Urun.java
@@ -0,0 +1,16 @@
+public class Urun {
+    protected int id;
+    protected String isim;
+    protected double fiyat;
+
+    public Urun (String isim, double fiyat){
+        this.isim=isim;
+        this.fiyat=fiyat;
+    }
+    
+    public double getFiyat(){
+        return fiyat;
+    }
+  
+    
+}
