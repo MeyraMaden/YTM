311bac0f1eaaa5033a07692cd75411dc23fcb99a
meyra <meyramaden@gmail.com>
2025-02-28 14:37:49 +0300

    main eklendi

---
+++ 2_O/csharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Eticaret2_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Kullanici ali = new StandartKullanici();
+            Kullanici veli = new PremiumKullanici();
+            ali.SetIsim("ali");
+            veli.SetIsim("veli");
+
+            Urun telefon = new Urun("telefon", 3000);
+            Urun bilgisayar = new Urun("bilgisayar", 5000);
+
+            ali.AddUrun(telefon);
+            ali.AddUrun(bilgisayar);
+            veli.AddUrun(telefon);
+            veli.AddUrun(bilgisayar);
+
+            Siparis.OdemeYap(ali);
+            Siparis.OdemeYap(veli);
+        }
+    }
+
+}
