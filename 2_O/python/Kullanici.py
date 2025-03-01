881187feb06e588c69fe84ef8abc038b4955be10
meyra <meyramaden@gmail.com>
2025-03-01 13:01:08 +0300

    kullanici classi eklendi

---
+++ 2_O/python/Kullanici.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import List
+from Urun import Urun
+
+class Kullanici(ABC):
+    def __init__(self):
+        self.id=None
+        self.isim=None
+        self.mail=None
+        self.urunler=[]
+
+    def get_toplam_tutar(self, fiyat):
+        pass
+    
+    def get_isim(self):
+        return self.isim    
+    def get_urunler(self):
+        return self.urunler
+    def get_urun(self, index):
+        return self.get_urunler()[index]
+    def add_urun(self, urun):
+        self.get_urunler().append(urun)
