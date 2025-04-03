355f85a66bdd49c59a58a23291448d21f3ff2bd5
meyra <meyramaden@gmail.com>
2025-04-03 23:12:15 +0300

    Added DBFactory class

---
+++ 2 Factory/chsarp/DBFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_csharp
+{
+    public class DBFactory
+    {
+        public const int MYSQL = 1;
+        public const int ORACLE = 2;
+        public const int MSSQL = 3;
+
+        public IDB GetDB(int type)
+        {
+            if (type == MYSQL)
+            {
+                return new MYSQL();
+            }
+            else if (type == ORACLE)
+            {
+                return new ORACLE();
+            }
+            else if (type == MSSQL)
+            {
+                return new MSSQL();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
