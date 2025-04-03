efca8313ff24544105c8e480af2c84ac9625f839
meyra <meyramaden@gmail.com>
2025-04-03 23:11:50 +0300

    Added IDB class

---
+++ 2 Factory/chsarp/IDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_csharp
+{
+    public interface IDB
+    {
+        public void SelectData();
+        public void InsertData();
+    }
+}
