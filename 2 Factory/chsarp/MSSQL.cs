20cbc0d238996f0c5bdb03e101ad3b08d084fec6
meyra <meyramaden@gmail.com>
2025-04-03 23:12:39 +0300

    Added Mssql class

---
+++ 2 Factory/chsarp/MSSQL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_csharp
+{
+    public class MSSQL : IDB
+    {
+        public void SelectData()
+        {
+            Console.WriteLine("MSSQL veritabanından veri okundu.");
+        }
+
+
+        public void InsertData()
+        {
+            Console.WriteLine("MSSQL veritabanına veri yazıldı.");
+        }
+    }
+}
