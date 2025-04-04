3658e3cd18c0b0865d235795708dac414e7df1ce
meyra <meyramaden@gmail.com>
2025-04-04 12:38:12 +0300

    Added oracle class

---
+++ 2 Factory/chsarp/ORACLE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_csharp
+{
+    public class ORACLE : IDB
+    {
+        public void SelectData()
+        {
+            Console.WriteLine("ORACLE veritabanından veri okundu.");
+        }
+
+        public void InsertData()
+        {
+            Console.WriteLine("ORACLE veritabanına veri yazıldı.");
+        }
+    }
+}
