7e4a313047a34fc54a3dc0a4bade4792b92bd984
meyra <meyramaden@gmail.com>
2025-04-04 12:37:26 +0300

    Added Mysql class

---
+++ 2 Factory/chsarp/MYSQL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_csharp
+{
+    public class MYSQL : IDB
+    {
+        public void SelectData()
+        {
+            Console.WriteLine("MYSQL veritabanından veri okundu.");
+        }
+
+        public void InsertData()
+        {
+            Console.WriteLine("MYSQL veritabanına veri yazıldı.");
+        }
+    }
+}
