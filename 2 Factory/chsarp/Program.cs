212af38858b5d19e0336ca8a9f174a0d080bd2b3
meyra <meyramaden@gmail.com>
2025-04-04 12:38:38 +0300

    Added main class

---
+++ 2 Factory/chsarp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_csharp
+{
+    public class Main
+    {
+        public static void main()
+        {
+            DBFactory dbFactory = new DBFactory();
+
+            IDB mysql = dbFactory.GetDB(DBFactory.MYSQL);
+            IDB oracle = dbFactory.GetDB(DBFactory.ORACLE);
+            IDB mssql = dbFactory.GetDB(3);
+
+            mysql.SelectData();
+            mysql.InsertData();
+
+            oracle.SelectData();
+            oracle.InsertData();
+
+            mssql.SelectData();
+            mssql.InsertData();
+        }
+    }
+}
